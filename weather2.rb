2def98e0695963bc25800c1a45b1d725263783df
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2015-01-16 14:25:18 +0100

    first version of rbWeather (uncomplete). added sprites for icons and its TXT version.

--- weather2.rb
+++ weather2.rb
@@ -10,9 +10,24 @@ require 'table_print'
 ## http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
 
 def get_weather(city)
-  url = "http://api.openweathermap.org/data/2.5/weather?q=#{c}&mode=json&units=metric"
+  url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&mode=json&units=metric"
   contents = open(url).read
   parsed_json = JSON.parse(contents)
+  if parsed_json['main'].nil? 
+    return nil
+  else
+    return parsed_json
+  end
+end
+
+def print_weather(data)
+  puts data["name"]
+  puts "---------------------------------------"
+  puts data["weather"][0]["description"]
+  puts data["weather"][0]["icon"]
+  puts "Temp. Act: #{data["main"]["temp"]}"
+  puts "Humidity:  #{data["main"]["humidity"]}%"
+  puts "Wind:      #{data["wind"]["speed"]}km/h"
 end
 
 
@@ -21,3 +36,9 @@ city = ARGV[0]
 opts = ARGV[1]
 
 
+dd = get_weather(city)
+if dd.nil?
+  puts "NIL?"
+else
+  print_weather(dd)
+end
